1a3eb3e12d59ea26fee8a3e422b90bfe92e1ac95
user <user@ubuntu>
2012-05-30 16:27:10 +0300

    Gemfile revision Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ofina::Application.config.secret_token = 'a2c59cc5a84561e813ef6cf31ae2fd141af7ec2bf4e2f2edaa0aaf1db7db70be1dd192928de46a16a6afb0652be8aee42e276efa911c34f33b42b2850a527a7a'
