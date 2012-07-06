42dd10d944a1ecfd273bdcf0a500beff84220d19
user <user@ubuntu>
2012-07-06 18:09:38 +0300

    CRUID для Enterprises

--- app/controllers/enterprises_controller.rb
+++ app/controllers/enterprises_controller.rb
@@ -8,30 +8,44 @@ class EnterprisesController < ApplicationController
   def show
     @enterprise=Enterprise.find(params[:id])
   end
-  
-  def delete
     
-  end
-  
   def edit
-    
+    @enterprise=Enterprise.find(params[:id])
   end
   
   def new
-    
+    @enterprise=Enterprise.new    
+  end  
+  
+  def delete
+    @enterprise=Enterprise.find(params[:id])
   end
   
-  
-  
   def create
-    
+    @enterprise=Enterprise.new(params[:enterprise])
+    @enterprise=current_user.enterprises.build(params[:enterprise])
+    if @enterprise.save
+      flash[:notice]="Предприятие создано."
+      redirect_to enterprises_path
+    else
+      render('new')
+    end
   end
   
   def update
-    
+    @enterprise=Enterprise.find(params[:id])
+    if @enterprise.update_attributes(params[:enterprise])
+      flash[:notice]="Предприятие обновлено."
+      redirect_to enterprise_path(@enterprise)
+    else  
+      render('edit')
+    end
   end
   
   def destroy
-    
+    enterprise=Enterprise.find(params[:id])
+    enterprise.destroy
+    flash[:notice]="Предприятие удалено."
+    redirect_to enterprises_path
   end
 end
