23655c1b459d56d92c92b4f0463e9df9b878dc11
user <user@ubuntu>
2012-07-16 17:50:36 +0300

    CRUID для формы 2

--- app/controllers/form_one_reports_controller.rb
+++ app/controllers/form_one_reports_controller.rb
@@ -19,14 +19,12 @@ class FormOneReportsController < ApplicationController
   
   def new
     @enterprise=Enterprise.find_by_id(params[:enterprise_id])
-    #@form_one_report=FormOneReport.new
     @form_one_report=@enterprise.form_one_reports.build
     @OrgName=@enterprise.org_name    
   end
   
   def create
     @enterprise=Enterprise.find_by_id(params[:enterprise_id])
-    # @form_one_report=FormOneReport.new(params[:form_one_report])
     @form_one_report=@enterprise.form_one_reports.build(params[:form_one_report])
     if @form_one_report.save
       flash[:notice]="Отчёт создан."
@@ -38,14 +36,12 @@ class FormOneReportsController < ApplicationController
   
   def edit
     @enterprise=Enterprise.find_by_id(params[:enterprise_id])
-    #@form_one_report=FormOneReport.find(params[:id])
     @form_one_report=@enterprise.form_one_reports.find_by_id(params[:id])
     @OrgName=@enterprise.org_name    
   end
 
   def update
     @enterprise=Enterprise.find_by_id(params[:enterprise_id])
-    #@form_one_report=FormOneReport.find(params[:id])
     @form_one_report=@enterprise.form_one_reports.find_by_id(params[:id])
     if @form_one_report.update_attributes(params[:form_one_report])
       flash[:notice]="Отчёт обновлен."
@@ -61,7 +57,6 @@ class FormOneReportsController < ApplicationController
     
   def destroy
     @enterprise=Enterprise.find_by_id(params[:enterprise_id])
-    #@form_one_report=FormOneReport.find(params[:id])
     @form_one_report=@enterprise.form_one_reports.find_by_id(params[:id])
     @form_one_report.destroy
     flash[:notice]="Отчёт удалён."
--- app/controllers/form_two_reports_controller.rb
+++ app/controllers/form_two_reports_controller.rb
@@ -2,27 +2,67 @@
 class FormTwoReportsController < ApplicationController
   before_filter :authenticate_user!
   
-  def edit
-  end
-
-  def show
-  end
-
   def index
+    @enterprise=Enterprise.find_by_id(params[:enterprise_id])
+    @form_two_reports=FormTwoReport.FormTwoEnterpriseFor(params[:enterprise_id]).Sorted.paginate(page: params[:page])
+    @OrgName=@enterprise.org_name
+  end
+  
+  def show
+    @enterprise=Enterprise.find_by_id(params[:enterprise_id])
+    @form_two_report=FormTwoReport.find(params[:id])    
+    @OrgName=@enterprise.org_name
+    @DatePeriod_beg=@form_two_report.date_period_beg
+    @DatePeriod_end=@form_two_report.date_period_end
+    @DatePeriod_beg_g4=(@DatePeriod_beg).prev_year
+    @DatePeriod_end_g4=(@DatePeriod_end).prev_year
+    @form_two_report_g4=FormTwoReport.where(["enterprise_id=? AND date_period_beg=? AND date_period_end=?",
+                  params[:enterprise_id],@DatePeriod_beg_g4,@DatePeriod_end_g4]).first # запись с данными за аналогичный период предыдущего года.
+  end
+  
+  def new
+    @enterprise=Enterprise.find_by_id(params[:enterprise_id])
+    @form_two_report=@enterprise.form_two_reports.build
+    @OrgName=@enterprise.org_name    
+  end
+  
+  def create
+    @enterprise=Enterprise.find_by_id(params[:enterprise_id])
+    @form_two_report=@enterprise.form_two_reports.build(params[:form_two_report])
+    if @form_two_report.save
+      flash[:notice]="Отчёт создан."
+      redirect_to enterprise_form_two_reports_path(@enterprise)
+    else
+      render('new')
+    end
+  end
+  
+  def edit
+    @enterprise=Enterprise.find_by_id(params[:enterprise_id])
+    @form_two_report=@enterprise.form_two_reports.find_by_id(params[:id])
+    @OrgName=@enterprise.org_name    
   end
 
   def update
+    @enterprise=Enterprise.find_by_id(params[:enterprise_id])
+    @form_two_report=@enterprise.form_two_reports.find_by_id(params[:id])
+    if @form_two_report.update_attributes(params[:form_two_report])
+      flash[:notice]="Отчёт обновлен."
+      redirect_to enterprise_form_two_report_path(@enterprise)
+    else  
+      render('edit')
+    end
   end
-
+  
   def delete
+    @form_two_report=FormTwoReport.find(params[:id])
   end
-
-  def new
-  end
-
+    
   def destroy
-  end
-
-  def create
+    @enterprise=Enterprise.find_by_id(params[:enterprise_id])
+    @form_two_report=@enterprise.form_two_reports.find_by_id(params[:id])
+    @form_two_report.destroy
+    flash[:notice]="Отчёт удалён."
+    redirect_to enterprise_form_two_reports_path
   end
 end
