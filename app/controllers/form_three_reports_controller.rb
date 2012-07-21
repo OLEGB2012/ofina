37c47a39148a0b79020416137ddeab20993abd36
user <user@ubuntu>
2012-07-22 01:14:46 +0300

    cruid для формы 3

--- app/controllers/form_three_reports_controller.rb
+++ app/controllers/form_three_reports_controller.rb
@@ -29,7 +29,19 @@ class FormThreeReportsController < ApplicationController
   def new
     @enterprise=Enterprise.find_by_id(params[:enterprise_id])
     @form_three_report=@enterprise.form_three_reports.build
-    @OrgName=@enterprise.org_name    
+    @OrgName=@enterprise.org_name
+#    @DatePeriod_beg     = @form_three_report.date_period_beg
+#    @DatePeriod_end     = @form_three_report.date_period_end
+#    @Date_For_S010      = ((@DatePeriod_beg).to_date.prev_year-1).end_of_year
+#    @Date_For_S040      = ((@DatePeriod_beg).to_date.prev_year-1).end_of_year
+#    @Date_From_For_S0401= (@DatePeriod_beg).to_date.prev_year
+#    @Date_To_For_S0401  = (@DatePeriod_end).to_date.prev_year
+#    @Date_For_S100      = (@DatePeriod_end).to_date.prev_year
+#    @Date_For_S110      = ((@DatePeriod_beg).to_date.prev_year).end_of_year
+#    @Date_For_S140      = ((@DatePeriod_beg).to_date.prev_year).end_of_year
+#    @Date_From_For_S1401= (@DatePeriod_beg).to_date
+#    @Date_To_For_S1401  = (@DatePeriod_end).to_date
+#    @Date_For_S200      = (@DatePeriod_end).to_date
   end
   
   def create
@@ -37,7 +49,7 @@ class FormThreeReportsController < ApplicationController
     @form_three_report=@enterprise.form_three_reports.build(params[:form_three_report])
     if @form_three_report.save
       flash[:notice]="Отчёт создан."
-      redirect_to enterprise_form_Three_reports_path(@enterprise)
+      redirect_to enterprise_form_three_reports_path(@enterprise)
     else
       render('new')
     end
@@ -46,7 +58,19 @@ class FormThreeReportsController < ApplicationController
   def edit
     @enterprise=Enterprise.find_by_id(params[:enterprise_id])
     @form_three_report=@enterprise.form_three_reports.find_by_id(params[:id])
-    @OrgName=@enterprise.org_name    
+    @OrgName=@enterprise.org_name
+#    @DatePeriod_beg     = @form_three_report.date_period_beg
+#    @DatePeriod_end     = @form_three_report.date_period_end
+#    @Date_For_S010      = ((@DatePeriod_beg).to_date.prev_year-1).end_of_year
+#    @Date_For_S040      = ((@DatePeriod_beg).to_date.prev_year-1).end_of_year
+#    @Date_From_For_S0401= (@DatePeriod_beg).to_date.prev_year
+#    @Date_To_For_S0401  = (@DatePeriod_end).to_date.prev_year
+#    @Date_For_S100      = (@DatePeriod_end).to_date.prev_year
+#    @Date_For_S110      = ((@DatePeriod_beg).to_date.prev_year).end_of_year
+#    @Date_For_S140      = ((@DatePeriod_beg).to_date.prev_year).end_of_year
+#    @Date_From_For_S1401= (@DatePeriod_beg).to_date
+#    @Date_To_For_S1401  = (@DatePeriod_end).to_date
+#    @Date_For_S200      = (@DatePeriod_end).to_date    
   end
 
   def update
