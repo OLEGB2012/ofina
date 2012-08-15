fa90b1919d65ddaf3cc40f09e264e9bd66815406
user <user@ubuntu>
2012-08-15 15:52:58 +0300

    Визуализация Расчёты, модель, миграция.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,7 @@ Ofina::Application.configure do
   # Use a different logger for distributed setups
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
   
-  config.logger = Logger.new('log/production.log', 'monthly')
+  config.logger = Logger.new('production.log', 'monthly')
 
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
