180d9375d7347a5a21e051e7ab0845ee9f94c2ac
user <user@ubuntu>
2012-10-03 04:18:41 +0300

    Деплоим...

--- config/application.rb
+++ config/application.rb
@@ -15,8 +15,8 @@ module Ofina
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     
-    #Using Devise on Heroku with Ruby on Rails 3.1 requires setting:
-    config.assets.initialize_on_precompile = false
+    #Using Devise on Heroku with Ruby on Rails 3.1 requires setting false:
+    config.assets.initialize_on_precompile = true
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
