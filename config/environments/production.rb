f16c1d4ac87dd82b8d6e0e0c59a4128ac6b24594
user <user@ubuntu>
2012-10-03 04:48:22 +0300

    Деплоим...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,8 @@ Ofina::Application.configure do
   
   # поставить какой надо хост для почты config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   
+  # Settings specified here will take precedence over those in config/application.rb
+
   # Code is not reloaded between requests
   config.cache_classes = true
 
@@ -11,13 +13,16 @@ Ofina::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
+  
+  #Using Devise on Heroku with Ruby on Rails 3.1 requires setting false:
+  config.assets.initialize_on_precompile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -27,7 +32,7 @@ Ofina::Application.configure do
 
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
-  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx 
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
