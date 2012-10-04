7703793964bb0280d55787eb944d5aa1d46b1de9
user <user@ubuntu>
2012-10-04 10:24:06 +0300

    Деплоим...

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,9 +13,9 @@ set :user,            "deployer"
 set :group,           "staff"
 set :use_sudo,        false
 
-role :web,    "localhost"
-role :app,    "localhost"
-role :db,     "localhost", :primary => true
+role :web,    "rvm"
+role :app,    "rvm"
+role :db,     "rvm", :primary => true
 
 set(:latest_release)  { fetch(:current_path) }
 set(:release_path)    { fetch(:current_path) }
