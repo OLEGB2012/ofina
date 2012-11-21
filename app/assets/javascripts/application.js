10abd18e4f2482b297afedb44286962d839bc2bc
user <user@ubuntu>
2012-11-22 02:38:39 +0300

    Aplying JQuery-Datepicker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,12 +9,12 @@
 //
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
-//= require bootstrap
 //= require jquery
 //= require jquery-ui
 //= require jquery_ujs
 //= require jquery.ui.datepicker
 //= require jquery.ui.datepicker-ru
+//= require bootstrap
 //= require_tree .
 
 $(function() {
